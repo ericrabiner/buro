302c1266724686cf07373ea46157a0eab40a59bd
Eric Rabiner <ericrabiner@gmail.com>
2020-07-19 21:52:41 -0400

    first

---
+++ server.js
@@ -0,0 +1,34 @@
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@buro.0gfrl.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("🚀 MongoDB Connected");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`🚀 Server running on port: ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
